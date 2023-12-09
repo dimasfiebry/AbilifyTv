69a7c0382560a43f7d35bf1c8105633ae089cb0f
mac <mac@macs-MacBook-Pro.local>
2023-12-10 00:17:51 +0700

    first commit

--- build/js/script.js
+++ build/js/script.js
@@ -1,39 +1,20 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Get the elements
-    var navbarAtas = document.querySelector('.navbar-atas');
-    var menuBawah = document.querySelector('.menu-bawah');
-    var twoLineButton = document.querySelector('.two-line');
-    
-    function toggleNavbarVisibility(event) {
-        event.preventDefault(); // Prevent the default behavior (page refresh)
-        if (navbarAtas.style.display === 'flex') {
-            navbarAtas.style.display = 'none';
-            menuBawah.style.display = 'flex';
-        } else {
-            navbarAtas.style.display = 'flex';
-            menuBawah.style.display = 'none';
-        }
-    }
-    if (navbarAtas && menuBawah && twoLineButton) {
-        function handleScroll() {
-            var scrollPosition = window.scrollY || window.pageYOffset;
 
-            if (scrollPosition < 462) {
-                navbarAtas.style.display = 'none';
-                menuBawah.style.display = 'flex';
-            } else if (scrollPosition < 590 ){
-                navbarAtas.style.display = 'flex';
-                menuBawah.style.display = 'none';
-            }
-        }
+let lastScrolltop = 0
+let navbar = document.querySelector('.navbar')
 
-        
 
-        window.addEventListener('scroll', handleScroll);
-        twoLineButton.addEventListener('click', toggleNavbarVisibility);
+window.addEventListener('scroll', ()=>{
+   let scrollTop = window.scrollY || document.documentElement.scrollTop
 
-        handleScroll();
-    } else {
-        console.error('Error broo');
-    }
-});
+  
+
+   if (scrollTop>lastScrolltop && scrollTop > navbar.offsetHeight) {
+     navbar.classList.add('hidden')
+
+   } else {
+      if(scrollTop + window.innerHeight < document.documentElement.scrollHeight) {
+        navbar.classList.remove('hidden')
+      }
+   }
+   lastScrolltop = scrollTop
+})
